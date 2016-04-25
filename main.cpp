8ba26c93f9d3521f3cae31701a85566f68b4095d
Luis96791 <luis.fajardozap@hotmail.com>
2016-04-25 12:05:26 -0600

    Pruebas ttf

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include "utility.h"
+#include <iostream>
+
+using namespace std;
 
 
 int main()
@@ -9,20 +12,16 @@ int main()
     sf::RenderWindow window;
     sf::Texture text_dado1,text_dado2,text_tablero, text_name;
     sf::Sprite back_dado1,back_dado2,back_tablero, back_name;
-//    sf::Font name;
-//    sf::Text texto_name;
-//    string nombre;
+    sf::Font name;
+    sf::Text texto_name;
+    string nombre = "Luis";
 
     window.create(sf::VideoMode(1360,690,32),"Monopoly",sf::Style::Close);
     window.setVerticalSyncEnabled(true);
 
-//    if(!name.loadFromFile("arial.ttf")){
-//
-//    }
+    if(!name.loadFromFile("arial.ttf")){
 
-//    texto_name.setFont(name);
-//    texto_name.setColor(sf::Color::Green);
-//    texto_name.setPosition(20,0);
+    }
 
     text_tablero.loadFromFile("tablero.png");
     back_tablero.setTexture(text_tablero);
@@ -30,6 +29,11 @@ int main()
     back_tablero.setPosition(0,0);
     back_dado1.setPosition(300,200);
     back_dado2.setPosition(500,200);
+
+    texto_name.setFont(name);
+    texto_name.setCharacterSize(72);
+    texto_name.setColor(sf::Color::Green);
+    texto_name.setPosition(20,0);
 
     while (window.isOpen())
     {
@@ -48,12 +52,12 @@ int main()
             }
         }
 
-//        texto_name.setString(cin>>nombre);
+        texto_name.setString("nombre");
 
-//        window.draw(texto_name);
         window.draw(back_tablero);
         window.draw(back_dado1);
         window.draw(back_dado2);
+        window.draw(texto_name);
 
         window.display();
     }
