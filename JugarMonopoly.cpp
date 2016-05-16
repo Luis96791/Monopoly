0b034acd56d9dacb6961aaff48da82fe28c81cd8
Luis96791 <luis.fajardozap@hotmail.com>
2016-05-16 15:07:24 -0600

    sistema de turnos 2/6

--- JugarMonopoly.cpp
+++ JugarMonopoly.cpp
@@ -19,11 +19,12 @@ vector<CrearJugador> JugarMonopoly::cargarJugadores()
 
 bool JugarMonopoly::mover(sf::Sprite ficha,int suma_dados)
 {
-    if(ficha.getPosition().y==640){
-        if(suma_dados>0){
-            return mover(ficha,suma_dados-1);
-        }
-    }
+//    if(ficha.getPosition().y==640){
+//        if(suma_dados>0){
+//            return mover(ficha,suma_dados-1);
+//        }
+//    }
+
 }
 
 void JugarMonopoly::ventanaTablero()
@@ -33,8 +34,7 @@ void JugarMonopoly::ventanaTablero()
     sf::Sprite back_tablero,back_dado_1,back_dado_2,back_ficha_1,back_ficha_2,back_ficha_3,back_ficha_4,back_ficha_5,back_ficha_6;
     sf::Vector2f mouse;
     sf::Mouse mouse_position;
-    int suma_dados,guardar_dado_1,guardar_dado_2,dado_prueba,m;
-
+    int suma_dados,guardar_dado_1,guardar_dado_2,dado_prueba,clicks=0;
     int getPositionX, getPositionY, dado_file, setPositionX, setPositionY;
 
     cargarJugadores();
@@ -49,14 +49,49 @@ void JugarMonopoly::ventanaTablero()
     text_dado_2.loadFromFile("dados/1.png");
     back_dado_2.setTexture(text_dado_2);
 
-    text_ficha_1.loadFromFile("fichas_tablero/ficha_azul.png");
-    back_ficha_1.setTexture(text_ficha_1);
-    back_ficha_1.setPosition(270,20);
-
-    text_ficha_2.loadFromFile("fichas_tablero/ficha_roja.png");
-    back_ficha_2.setTexture(text_ficha_2);
-    back_ficha_2.setPosition(640,640);
-    //cout<<suma_dados<<endl;
+    ifstream cargar("jugadores.txt");
+    string n, f;
+    bool ficha1,ficha2,ficha3,ficha4,ficha5,ficha6,turno;
+    while(cargar>>n&&cargar>>f){
+            cout<<f<<endl;
+        if(f=="ficha_azul"){
+            text_ficha_1.loadFromFile("fichas_tablero/ficha_azul.png");
+            back_ficha_1.setTexture(text_ficha_1);
+            back_ficha_1.setPosition(640,640);
+            ficha1=true;
+        }
+        if(f=="ficha_roja"){
+            text_ficha_2.loadFromFile("fichas_tablero/ficha_roja.png");
+            back_ficha_2.setTexture(text_ficha_2);
+            back_ficha_2.setPosition(640,640);
+            ficha2=true;
+        }
+        if(f=="ficha_verde"){
+            text_ficha_3.loadFromFile("fichas_tablero/ficha_verde.png");
+            back_ficha_3.setTexture(text_ficha_3);
+            back_ficha_3.setPosition(640,640);
+            ficha3=true;
+        }
+        if(f=="ficha_amarilla"){
+            text_ficha_4.loadFromFile("fichas_tablero/ficha_amarilla.png");
+            back_ficha_4.setTexture(text_ficha_4);
+            back_ficha_4.setPosition(640,640);
+            ficha4=true;
+        }
+        if(f=="ficha_blanca"){
+            text_ficha_5.loadFromFile("fichas_tablero/ficha_blanca.png");
+            back_ficha_5.setTexture(text_ficha_5);
+            back_ficha_5.setPosition(640,640);
+            ficha5=true;
+        }
+        if(f=="ficha_naranja"){
+            text_ficha_6.loadFromFile("fichas_tablero/ficha_naranja.png");
+            back_ficha_6.setTexture(text_ficha_6);
+            back_ficha_6.setPosition(640,640);
+            ficha6=true;
+        }
+    }
+    cargar.close();
 
     back_dado_1.setPosition(1140,580);
     back_dado_2.setPosition(1250,580);
@@ -74,6 +109,7 @@ void JugarMonopoly::ventanaTablero()
 
             if(utility.clickSprite(back_dado_1,mouse)||utility.clickSprite(back_dado_2,mouse))
             {
+                cout<<clicks++<<endl;
                 guardar_dado_1=utility.dadoUno();
                 guardar_dado_2=utility.dadoDos();
                 suma_dados=guardar_dado_1+guardar_dado_2;
@@ -88,16 +124,6 @@ void JugarMonopoly::ventanaTablero()
 
             //PROBANDO TABLERO
 
-            ifstream out("setPositions.txt");
-            while(out>>getPositionX&&out>>getPositionY&&out>>dado_file&&out>>setPositionX&&out>>setPositionY){
-                if(back_ficha_1.getPosition().x==getPositionX&&back_ficha_1.getPosition().y==getPositionY&&suma_dados==dado_file){
-                    cout<<"entrando..."<<endl;
-                    cout<<suma_dados<<endl;
-                    back_ficha_1.setPosition(setPositionX,setPositionY);
-                    suma_dados=0;
-                }
-            }
-            out.close();
         }
 
 //        if(mover(back_ficha_2,5)){
@@ -105,17 +131,44 @@ void JugarMonopoly::ventanaTablero()
 //            back_ficha_2.setPosition(m-85,640);
 //            cout<<back_ficha_2.getPosition().x<<endl;
 //        }
+        if(clicks%2==0){
+            ifstream out1("setPositions.txt");
+            while(out1>>getPositionX&&out1>>getPositionY&&out1>>dado_file&&out1>>setPositionX&&out1>>setPositionY){
+                if(back_ficha_1.getPosition().x==getPositionX&&back_ficha_1.getPosition().y==getPositionY&&suma_dados==dado_file){
+                    cout<<"entrando..."<<endl;
+                    cout<<suma_dados<<endl;
+                    back_ficha_1.setPosition(setPositionX,setPositionY);
+                    suma_dados=0;
+                }
+            }
+            out1.close();
+        }else{
+            ifstream out2("setPositions.txt");
+            while(out2>>getPositionX&&out2>>getPositionY&&out2>>dado_file&&out2>>setPositionX&&out2>>setPositionY){
+                if(back_ficha_2.getPosition().x==getPositionX&&back_ficha_2.getPosition().y==getPositionY&&suma_dados==dado_file){
+                    cout<<"entrando..."<<endl;
+                    cout<<suma_dados<<endl;
+                    back_ficha_2.setPosition(setPositionX,setPositionY);
+                    suma_dados=0;
+                }
+            }
+            out2.close();
+        }
 
         window.draw(back_tablero);
         window.draw(back_dado_1);
         window.draw(back_dado_2);
-        window.draw(back_ficha_1);
-        window.draw(back_ficha_2);
+        if(ficha1){window.draw(back_ficha_1);}
+        if(ficha2){window.draw(back_ficha_2);}
+        if(ficha3){window.draw(back_ficha_3);}
+        if(ficha4){window.draw(back_ficha_4);}
+        if(ficha5){window.draw(back_ficha_5);}
+        if(ficha6){window.draw(back_ficha_6);}
         window.display();
     }
 }
 
 JugarMonopoly::~JugarMonopoly()
 {
-//    remove(nombre_archivo.c_str());
+    remove(nombre_archivo.c_str());
 }
